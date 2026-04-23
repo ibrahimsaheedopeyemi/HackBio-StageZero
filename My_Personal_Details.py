9d03c7db610fd876f0bd519c1208dd5087f6e113
Ibrahim Saheed Opeyemi <136084476+ibrahimsaheedopeyemi@users.noreply.github.com>
2026-04-23 15:42:39 +0100

    Add files via upload

---
+++ My_Personal_Details.py
@@ -0,0 +1,10 @@
+# Personal details
+name = "Ibrahim Saheed Opeyemi"
+affiliation = "University of Ilorin"
+
+# Favourite gene and organism
+favourite_gene = "rpoB"
+organism = "Mycobacterium tuberculosis"
+
+# Output statement
+print(f"Hi, my name is {name}, a graduate assistant at the {affiliation}. \nMy favourite gene is {favourite_gene} in {organism}.")
